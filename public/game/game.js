3d178e3e64795ff885c64a9832e9758d69aa1fb5
hdf1986 <hugo.farji+github@gmail.com>
2019-09-06 20:14:43 -0300

    NOW THE ARE JOINED

--- public/game/game.js
+++ public/game/game.js
@@ -2,19 +2,36 @@
   const $canvas = document.querySelector('.canvas');
   const context = $canvas.getContext('2d');
   const socket = io()
+  const dots = []
 
-  const drawPoint = (x, y) => {
-    context.strokeStyle = "#ffa200"
-    context.lineWidth = 5
-    context.beginPath()
-    context.moveTo(x, y)
-    context.lineTo(x + 1, y + 1)
-    context.stroke()
+  const drawPoint = (x, y, clicked) => {
+    dots.push({ x, y, clicked })
   }
+  const reDraw = (x, y) => {
+    context.clearRect(0, 0, $canvas.width, $canvas.height);
+    if(dots.length < 2) return requestAnimationFrame(reDraw);
+
+    dots.forEach(({x, y, clicked}, index) => {
+      if(!clicked || !index) return;
+      const { x: previousX, y: previousY } = dots[index - 1]
+      context.strokeStyle = "#ffa200";
+      context.lineJoin = 'round';
+      context.lineWidth = 5;
+
+      context.beginPath();
+      context.moveTo(previousX, previousY)
+      context.lineTo(x, y);
+      context.closePath();
+      context.stroke();
+    })
+    requestAnimationFrame(reDraw)
+  }
+  requestAnimationFrame(reDraw)
   
-  socket.on('draw', ({x, y}) => {
-    drawPoint(x, y)
+  socket.on('draw', ({x, y, clicked}) => {
+    drawPoint(x, y, clicked)
   })
+  requestAnimationFrame(() => console.log('12'))
 
   fetch('/points')
     .then(res => res.json())
